2019b69a054f189dde4ce4a49d34466440874b88
Megharaj S H <msh@vmware.com>
2020-04-27 11:43:20 +0530

    Initial commit

---
+++ src/app/strength/stength.pipe.spec.ts
@@ -0,0 +1,15 @@
+import {StrengthPipe} from "./strength.pipe";
+
+describe('StrengthPipe', () => {
+    it ('should display week if strength is 5', () => {
+        let pipe = new StrengthPipe();
+        let val = pipe.transform(5);
+        expect(val).toEqual('5 (weak)');
+    })
+
+    it ('should display strong if strength is 10', () => {
+        let pipe = new StrengthPipe();
+        let val = pipe.transform(10);
+        expect(val).toEqual('10 (strong)');
+    })
+})
